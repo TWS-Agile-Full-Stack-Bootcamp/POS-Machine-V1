c14a954e62bcca5d694280ec12f320e014830693
Zhen She <neal.seah@gmail.com>
2020-11-03 00:03:36 +0800

    feat: implement printReceipt

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,6 +1,15 @@
 import {loadAllItems, loadPromotions} from './Dependencies'
 
-export function printReceipt(tags: string[]): string {
+interface Item {
+  barcode: string
+  name: string
+  unit: string
+  price: number
+  count: number
+  subtotal: number
+}
+
+const renderReceipt = (items: Item[]): string => {
   return `***<store earning no money>Receipt ***
 Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
 Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
@@ -9,4 +18,16 @@ Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00
 Total：58.50(yuan)
 Discounted prices：7.50(yuan)
 **********************`
+}
+
+const promote = (items: Item[]): Item[] => {
+  return [<Item>{}]
+}
+
+const decodeTag = (tags: string[]): Item[] => {
+  return [<Item>{}]
+}
+
+export function printReceipt(tags: string[]): string {
+  return renderReceipt(promote(decodeTag(tags)))
 }
