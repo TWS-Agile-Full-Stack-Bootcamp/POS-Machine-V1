ef6ed03954135e901929ebed140d99936557d6e9
Zhen She <neal.seah@gmail.com>
2020-11-04 21:40:00 +0800

    refactor: extract class ReceiptPrinter

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -19,78 +19,80 @@ interface Promotion {
   promote: Function
 }
 
-const renderUnit = (item: Item): string => item.unit + ((item.quantity > 1) ? 's' : '')
+export class ReceiptPrinter {
+  renderUnit = (item: Item): string => item.unit + ((item.quantity > 1) ? 's' : '')
 
-const renderFloat = (target: number): string => target.toFixed(2)
+  renderFloat = (target: number): string => target.toFixed(2)
 
-const renderItems = (items: Item[]): string => {
-  return items
-    .map(item => `Name：${item.name}，Quantity：${item.quantity} ${renderUnit(item)}，Unit：${renderFloat(item.price)}(yuan)，Subtotal：${renderFloat(item.subtotal)}(yuan)`)
-    .join('\n')
-}
+  renderItems = (items: Item[]): string => {
+    return items
+      .map(item => `Name：${item.name}，Quantity：${item.quantity} ${this.renderUnit(item)}，Unit：${this.renderFloat(item.price)}(yuan)，Subtotal：${this.renderFloat(item.subtotal)}(yuan)`)
+      .join('\n')
+  }
 
-const sum = (a: number, b: number): number => a + b
+  sum = (a: number, b: number): number => a + b
 
-const calculateTotal = (items: Item[]): number => {
-  return items.map(item => item.subtotal).reduce(sum, 0)
-}
+  calculateTotal = (items: Item[]): number => {
+    return items.map(item => item.subtotal).reduce(this.sum, 0)
+  }
 
-const calculateDiscount = (items: Item[]): number => {
-  return items.map(item => item.price * item.quantity - item.subtotal).reduce(sum, 0)
-}
+  calculateDiscount = (items: Item[]): number => {
+    return items.map(item => item.price * item.quantity - item.subtotal).reduce(this.sum, 0)
+  }
 
-const renderReceipt = (items: Item[]): string => {
-  return `***<store earning no money>Receipt ***
-${renderItems(items)}
+  renderReceipt = (items: Item[]): string => {
+    return `***<store earning no money>Receipt ***
+${this.renderItems(items)}
 ----------------------
-Total：${renderFloat(calculateTotal(items))}(yuan)
-Discounted prices：${renderFloat(calculateDiscount(items))}(yuan)
+Total：${this.renderFloat(this.calculateTotal(items))}(yuan)
+Discounted prices：${this.renderFloat(this.calculateDiscount(items))}(yuan)
 **********************`
-}
+  }
 
-const byBarcodeOf = (target: {barcode: string}) => (({barcode}: {barcode: string}) => barcode === target.barcode)
+  byBarcodeOf = (target: {barcode: string}) => (({barcode}: {barcode: string}) => barcode === target.barcode)
 
-export const promote = (items: Item[]): Item[] => {
-  const promotions: Promotion[] = loadPromotions()
-  const discounts = promotions.flatMap(({barcodes, promote}) => items
-    .filter(item => barcodes.includes(item.barcode))
-    .map(item => ({
-      barcode: item.barcode,
-      subtotal: promote(item)
-    })))
-  return items.map(item => {
-    const theDiscount = discounts.find(byBarcodeOf(item))
-    return theDiscount ? Object.assign(item, {subtotal: theDiscount.subtotal}) : item
-  })
-}
+  promote (items: Item[]): Item[] {
+    const promotions: Promotion[] = loadPromotions()
+    const discounts = promotions.flatMap(({barcodes, promote}) => items
+      .filter(item => barcodes.includes(item.barcode))
+      .map(item => ({
+        barcode: item.barcode,
+        subtotal: promote(item)
+      })))
+    return items.map(item => {
+      const theDiscount = discounts.find(this.byBarcodeOf(item))
+      return theDiscount ? Object.assign(item, {subtotal: theDiscount.subtotal}) : item
+    })
+  }
 
-export const expandItemFromBarcode = (tags: Tag[]): Item[] => {
-  const skus: SKU[] = loadAllItems()
-  return tags.map(tag => {
-    const theSku = skus.find(byBarcodeOf(tag))
-    return {
-      barcode: theSku!.barcode,
-      name: theSku!.name,
-      unit: theSku!.unit,
-      price: theSku!.price,
-      quantity: tag.quantity,
-      subtotal: tag.quantity * theSku!.price
-    }
-  })
-}
+  expandItemFromBarcode (tags: Tag[]): Item[] {
+    const skus: SKU[] = loadAllItems()
+    return tags.map(tag => {
+      const theSku = skus.find(this.byBarcodeOf(tag))
+      return {
+        barcode: theSku!.barcode,
+        name: theSku!.name,
+        unit: theSku!.unit,
+        price: theSku!.price,
+        quantity: tag.quantity,
+        subtotal: tag.quantity * theSku!.price
+      }
+    })
+  }
 
-export const decodeTags = (tags: string[]): Tag[] => {
-  const decodedTags: Tag[] = []
-  tags.forEach(tagString => {
-    const [barcode, quantityString] = tagString.split('-')
-    const quantity = quantityString ? parseFloat(quantityString) : 1
-    const found = decodedTags.find(byBarcodeOf({barcode}))
-    if (!found) decodedTags.push({barcode, quantity})
-    else found.quantity += quantity
-  })
-  return decodedTags
-}
+  decodeTags (tags: string[]): Tag[] {
+    const decodedTags: Tag[] = []
+    tags.forEach(tagString => {
+      const [barcode, quantityString] = tagString.split('-')
+      const quantity = quantityString ? parseFloat(quantityString) : 1
+      const found = decodedTags.find(this.byBarcodeOf({barcode}))
+      if (!found) decodedTags.push({barcode, quantity})
+      else found.quantity += quantity
+    })
+    return decodedTags
+  }
 
-export function printReceipt(tags: string[]): string {
-  return renderReceipt(promote(expandItemFromBarcode(decodeTags(tags))))
+  public printReceipt (tags: string[]): string {
+    return this.renderReceipt(this.promote(this.expandItemFromBarcode(this.decodeTags(tags))))
+  }
 }
