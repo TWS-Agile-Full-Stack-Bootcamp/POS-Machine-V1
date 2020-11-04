a287ea6040011f0837298f0902c1e9103a2b5040
Chen Xu <xuchen@thoughtworks.com>
2020-11-04 17:41:32 +0800

    feat: implement

--- src/PrintReceipt.ts
+++ src/PrintReceipt.ts
@@ -1,12 +1,127 @@
-import {loadAllItems, loadPromotions} from './Dependencies'
+import {loadAllItems, loadPromotions, Item} from './Dependencies'
+import {cursorTo} from 'readline'
+
+function parseTagsToItems(tags: string[]) {
+  return tags.map(tag => {
+    const splitedTags = tag.split('-')
+    if (splitedTags.length === 2) {
+      const result = new OrderItem(splitedTags[0], parseFloat(splitedTags[1]))
+      return result
+    }
+    return new OrderItem(tag, 1)
+  })
+}
+
+function sumAllItems(allItems: OrderItem[]): Array<OrderItem> {
+  return allItems.reduce((items: Array<OrderItem>, current: OrderItem) => {
+    let findItem = items.find(item => item.barcode === current.barcode)
+    if (!findItem) {
+      findItem = new OrderItem(current.barcode, 0)
+      items.push(findItem)
+    }
+    findItem.quantity += current.quantity
+    return items
+  }, Array<OrderItem>())
+}
+
+function getQuantityOrders(calculatedItems: Array<OrderItem>, allItems: ({ unit: string; price: number; name: string; barcode: string } | { unit: string; price: number; name: string; barcode: string } | { unit: string; price: number; name: string; barcode: string } | { unit: string; price: number; name: string; barcode: string } | { unit: string; price: number; name: string; barcode: string } | { unit: string; price: number; name: string; barcode: string })[]) {
+  return calculatedItems.reduce((orders: Array<PromotionOrder>, current: OrderItem) => {
+    let findOrderItem = orders.find(order => order.barcode === current.barcode)
+
+    if (!findOrderItem) {
+      let findItem = allItems.find(item => item.barcode === current.barcode)
+      if (!findItem) {
+        findItem = new Item('', '', '', 0)
+      }
+      findOrderItem = new PromotionOrder(current.barcode, findItem.name, findItem.price, findItem.unit)
+      orders.push(findOrderItem)
+    }
+    findOrderItem.quantity += current.quantity
+    return orders
+  }, new Array<PromotionOrder>())
+}
+
+function renderOrderReceipt(quantityOrders: Array<PromotionOrder>) {
+  return quantityOrders.reduce((pre, currect) => {
+    pre.push(currect.render())
+    return pre
+  }, new Array<string>()).join('\n')
+}
+
+function getSumSaving(quantityOrders: Array<PromotionOrder>) {
+  return quantityOrders.reduce((pre: number, current: PromotionOrder) => {
+    pre += current.saving
+    return pre
+  }, 0)
+}
+
+function getSumTotal(quantityOrders: Array<PromotionOrder>) {
+  return quantityOrders.reduce((pre: number, currnet: PromotionOrder) => {
+    pre += currnet.subTotal
+    return pre
+  }, 0)
+}
 
 export function printReceipt(tags: string[]): string {
-  return `***<store earning no money>Receipt ***
-Name：Sprite，Quantity：5 bottles，Unit：3.00(yuan)，Subtotal：12.00(yuan)
-Name：Litchi，Quantity：2.5 pounds，Unit：15.00(yuan)，Subtotal：37.50(yuan)
-Name：Instant Noodles，Quantity：3 bags，Unit：4.50(yuan)，Subtotal：9.00(yuan)
-----------------------
-Total：58.50(yuan)
-Discounted prices：7.50(yuan)
-**********************`
+  const items = parseTagsToItems(tags)
+  const calculatedItems = sumAllItems(items)
+
+  const allItems = loadAllItems()
+  const promotions = loadPromotions()
+
+  const quantityOrders = getQuantityOrders(calculatedItems, allItems)
+
+  const sumTotal = getSumTotal(quantityOrders)
+
+  const  sumSaving = getSumSaving(quantityOrders)
+
+  let receipt = '***<store earning no money>Receipt ***\n'
+  receipt += renderOrderReceipt(quantityOrders)
+
+  receipt += '\n----------------------\n'
+
+  receipt +=  `Total：${sumTotal.toFixed(2)}(yuan)\n`
+
+  receipt +=  `Discounted prices：${sumSaving.toFixed(2)}(yuan)\n`
+
+  receipt +=  '**********************'
+
+  return receipt
+}
+
+class OrderItem {
+  constructor(public barcode: string, public quantity: number) {
+  }
+}
+
+class PromotionOrder {
+    public quantity: number
+    private readonly halfPromotion = 0.5
+
+    constructor(public  barcode: string, public  name: string, public price: number, public  unit: string) {
+      this.quantity = 0
+    }
+
+    public render(): string {
+      return `Name：${this.name}，Quantity：${this.quantity} ${this.unit}s，Unit：${this.price.toFixed(2)}(yuan)，Subtotal：${this.subTotal.toFixed(2)}(yuan)`
+    }
+
+    public get saving(): number {
+      const allPromotions = loadPromotions()
+      const promotion = allPromotions.find(promotion => promotion.barcodes.findIndex(barcode => barcode === this.barcode) > 0)
+
+      if (promotion) {
+        return Math.floor(this.quantity / 3) * this.price
+      } else {
+        return 0
+      }
+    }
+
+    public get subTotal(): number {
+      return this.total - this.saving
+    }
+
+    public get total(): number {
+      return this.price * this.quantity
+    }
 }
